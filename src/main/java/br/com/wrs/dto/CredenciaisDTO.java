9493419e7625e562d36855e24f565d5347a0d569
vinicius <viniciusamalia@gmail.com>
2023-12-19 12:55:36 -0300

    Implementaçâo do JPA

--- src/main/java/br/com/wrs/dto/CredenciaisDTO.java
+++ src/main/java/br/com/wrs/dto/CredenciaisDTO.java
@@ -17,38 +17,7 @@ public class CredenciaisDTO {
 	private String senha;
 	private String email;
 	private Set<Integer> perfis = new HashSet<>();
-	
-	
-	
 
-	public Integer getUsrId() {
-		return usrId;
-	}
-	public void setUsrId(Integer usrId) {
-		this.usrId = usrId;
-	}
-	public String getUsuario() {
-		return usuario;
-	}
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}		
-
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public void setPerfis(Set<Integer> perfis) {
-		this.perfis = perfis;
-	}
 	public Set<Perfil> getPerfis() {
 		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
 	}
